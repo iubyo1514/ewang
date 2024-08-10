173e91464dc41f152f587638e076c5f39379b2de
iubyo1514 <iubyo1514@gmail.com>
2024-08-10 19:38:17 +0900

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext.commands import Bot,when_mentioned
+from discord import Intents,Embed
+
+
+BOT_TOKEN = 'MTI3MTc3MzgxMTQ4MTY0MTAyMg.Ggkcbg.ddbUiKwZsOJPROq4olZlOzIOXm9x1OTjrYvAjE'
+
+intents = Intents()
+intents.members = True
+intents.guilds = True
+
+bot = Bot(when_mentioned,intents=intents)
+
+@bot.event
+async def on_member_join(member:discord.member.Member):
+    embed = Embed(colour=0x70ff8f,title=f'새로운 유저가 PIXEL에 접속하셨어요!',description=f'{member.display_name}이(가) PIXEL의 {member.guild.member_count}번째 유저가 되셨습니다!')
+    await member.guild.get_channel(1271773665893027901).send(embed=embed)
+
+bot.run(BOT_TOKEN)
