07cb1f2e60e797b8ecb74452b501a617c77db483
iubyo1514 <iubyo1514@gmail.com>
2024-08-10 19:51:15 +0900

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext.commands import Bot,when_mentioned
 from discord import Intents,Embed
-
+import os
 
 BOT_TOKEN = 'MTI3MTc3MzgxMTQ4MTY0MTAyMg.Ggkcbg.ddbUiKwZsOJPROq4olZlOzIOXm9x1OTjrYvAjE'
 
@@ -16,4 +16,5 @@ async def on_member_join(member:discord.member.Member):
     embed = Embed(colour=0x70ff8f,title=f'새로운 유저가 PIXEL에 접속하셨어요!',description=f'{member.display_name}이(가) PIXEL의 {member.guild.member_count}번째 유저가 되셨습니다!')
     await member.guild.get_channel(1271773665893027901).send(embed=embed)
 
+acces_token = os.environ["BOT_TOKEN"]
 bot.run(BOT_TOKEN)
